84ad5ef569c6f1e2db292db76947291102681652
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-09-02 17:23:32 +0300

    Initial Commit

---
+++ iMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMovie
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
